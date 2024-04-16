4daca1195756464a663fd869272829663bcc09de
Dhaval Ladani <Dhaval@techerudite.com>
2024-04-16 14:11:45 +0530

    dark mode bug fix

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,7 @@ const { themeVariants, prefersLight, prefersDark } = require("tailwindcss-theme-
 
 /** @type {import('tailwindcss').Config} */
 export default {
+   darkMode: 'selector',
    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
    theme: {
       fontFamily: {
