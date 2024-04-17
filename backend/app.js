67e7733f0f1f8e70e22e5c862387ac9315da50f2
Dhaval Ladani <Dhaval@techerudite.com>
2024-04-17 12:38:51 +0530

    removed unused deps in backend

--- backend/app.js
+++ backend/app.js
@@ -4,23 +4,15 @@ const cors = require('cors');
 const projectRoutes = require('./routes/project');
 const authRoutes = require('./routes/auth');
 const mongoose = require('mongoose');
-const helmet = require('helmet');
-const morgan = require('morgan');
 require('dotenv').config();
-const fs = require('fs');
 const { DB_URL } = require('./utils/config');
 const { notFound, sendError } = require('./controllers/error');
-const path = require('path');
 
 const app = express();
-
-const logStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' }); // flags - a means new lines will be appended
 
 // useful middlewares
 app.use(cors());
 app.use(bodyParser.json());
-app.use(helmet());
-app.use(morgan('combined', { stream: logStream }));
 
 // main routes
 app.use('/auth', authRoutes);
